20902f2526e2f39b31751cf14a04a10d887aa0b8
suiyou <suiyouKSY108@gmail.com>
2022-12-02 14:14:44 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
  
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
